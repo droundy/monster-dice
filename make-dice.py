00ce1f048a745ac68cbb2afd8f04984aaf170699
David Roundy <roundyd@physics.oregonstate.edu>
2017-07-13 13:16:34 -0700

    fit icons more cleanly

--- make-dice.py
+++ make-dice.py
@@ -100,25 +100,28 @@ safesz = 1.0
 lw = .25
 
 def plotshield(sz,x0,y0,color):
+    sz *= .8 # shrink a bit vs original design
     x = np.linspace(x0-sz/2,x0+sz/2,100)
     X = 2*(x-x0)/sz
-    yshield = y0 + sz*(.5 - .6*(np.sqrt(1-X**8)) + 0.5*(abs(X)-1))
+    yshield = y0 + sz*(.5 - .6*(np.sqrt(abs(1-X**8))) + 0.5*(abs(X)-1))
     plt.fill_between(x,yshield,y0+sz/2, facecolor=color)
 
 def plotheal(sz,x00,y00,color):
     R = sz*.275/.5
     x0 = .4*sz
-    x = np.linspace(-R/1000,R+x0,100)
+    x = np.linspace(-R-x0,R+x0,200)
     dx = x[1]-x[0]
-    ytop = np.sqrt(R**2 - (x-x0)**2) -.02*sz
-    ybot = -np.sqrt(R**2 - (x-x0)**2) -.02*sz
+    ytop = np.zeros_like(x)
+    ytop[x>=0] = np.sqrt(abs(R**2 - (x[x>=0]-x0)**2))
+    ytop[x<=0] = np.sqrt(abs(R**2 - (x[x<=0]+x0)**2))
+    ybot = -ytop.copy()
     i=95
     while i >= 0:
-        if ybot[i] > ybot[i+1] - dx*.7:
-            ybot[i] = 2*ybot[i+1] - ybot[i+2]
+        if ybot[100+i] > ybot[100+i+1] - dx*.7:
+            ybot[100+i] = 2*ybot[100+i+1] - ybot[100+i+2]
+            ybot[100-i] = ybot[100+i]
         i-=1
     plt.fill_between(x00+x,y00+ybot,y00+ytop, facecolor=color)
-    plt.fill_between(x00-x,y00+ybot,y00+ ytop, facecolor=color)
 
 def plotrange(sz,color):
     R = sz*.275/.5
@@ -130,13 +133,24 @@ def plotrange(sz,color):
     plt.fill_between(x,ybot,ytop, facecolor=color)
 
 def plotwing(sz, x0, y0, color):
-    x = np.linspace(-sz,sz,100)
-    X = abs(x/sz)
-    wingtop = y0-.5*sz + 0.8*(1 + 4*X - np.sqrt((4*X)**2 + 1))
+    x = np.linspace(-sz*.6,sz*.6,100)
+    X = abs(x/max(x))
+    wingtop = 0.5*(1 + 4*X - np.sqrt((4*X)**2 + 1))
     wingbot = wingtop - 0.3*(.2+X)*np.sqrt(1-X)
-    plt.fill_between(1.1*x+x0,wingtop*sz,wingbot*sz, facecolor=color)
-    plt.fill_between(.9*x+x0,.8*wingtop*sz,.8*wingbot*sz, facecolor=color)
-    plt.fill_between(.8*x+x0,.65*wingtop*sz,.65*wingbot*sz, facecolor=color)
+    yoff = 0.0*sz
+    plt.fill_between(1.1*x+x0,
+                     y0+wingtop*sz+yoff,
+                     y0+wingbot*sz+yoff, facecolor=color)
+    plt.fill_between(.9*x+x0,
+                     y0+.8*wingtop*sz+yoff,
+                     y0+.8*wingbot*sz+yoff, facecolor=color)
+    wingbot = wingtop - 0.6*(.2+X)*np.sqrt(1-X)
+    plt.fill_between(.8*x+x0,
+                     y0+.6*wingtop*sz+yoff,
+                     y0+.6*wingbot*sz+yoff, facecolor=color)
+    plt.fill_between(.7*x+x0,
+                     y0+.4*wingtop*sz+yoff,
+                     y0+.4*wingbot*sz+yoff, facecolor=color)
 
 def plotretaliate(sz,x,y,color):
     theta = np.linspace(0, 0.8*2*np.pi, 200)
@@ -152,14 +166,12 @@ def plotretaliate(sz,x,y,color):
 def plotplus(sz, x, y, color, lw):
     plt.plot([x+sz/2, x-sz/2], [y,y], '-', lw=lw, color=color)
     plt.plot([x, x], [y+sz/2,y-sz/2], '-', lw=lw, color=color)
-    plt.plot([x+3*sz/8, x+sz/2], [y+sz/8,y], '-', lw=lw, color=color)
-    plt.plot([x+3*sz/8, x+sz/2], [y-sz/8,y], '-', lw=lw, color=color)
-    plt.plot([x-3*sz/8, x-sz/2], [y+sz/8,y], '-', lw=lw, color=color)
-    plt.plot([x-3*sz/8, x-sz/2], [y-sz/8,y], '-', lw=lw, color=color)
-    plt.plot([x+sz/8, x], [y+3*sz/8,y+sz/2], '-', lw=lw, color=color)
-    plt.plot([x-sz/8, x], [y+3*sz/8,y+sz/2], '-', lw=lw, color=color)
-    plt.plot([x+sz/8, x], [y-3*sz/8,y-sz/2], '-', lw=lw, color=color)
-    plt.plot([x-sz/8, x], [y-3*sz/8,y-sz/2], '-', lw=lw, color=color)
+    r = np.linspace(0.7*sz/2, sz/2, 2)
+    for theta in np.arange(0, 2*np.pi, np.pi/2):
+        plt.plot(x+r*np.cos(np.linspace(theta-.2, theta,2)),
+                 y+r*np.sin(np.linspace(theta-.2, theta,2)), '-', lw=lw, color=color)
+        plt.plot(x+r*np.cos(np.linspace(theta+.2, theta,2)),
+                 y+r*np.sin(np.linspace(theta+.2, theta,2)), '-', lw=lw, color=color)
 
 def plotspecial(sz, x, y, color, lw):
     theta = np.linspace(0,  2*np.pi, 4)
@@ -167,6 +179,7 @@ def plotspecial(sz, x, y, color, lw):
     plt.plot(x+r*np.sin(theta), y+r*np.cos(theta), '-', color=color, lw=lw)
 
 def gridtwo(sz, x, y, color, lw):
+    sz *= .9 # leave a bit of space...
     for off in [-.1, -.3, .1, .3]:
         plt.plot([x+sz/2, x-sz/2], [y+sz*off,y+sz*off], '-', lw=lw, color=color)
         plt.plot([x+sz*off, x+sz*off], [y+sz/2,y-sz/2], '-', lw=lw, color=color)
@@ -175,31 +188,28 @@ def gridtwo(sz, x, y, color, lw):
         plt.plot([x+sz*off, x+sz*off], [y+sz*.3,y-sz*.3], '-', lw=lw, color=color)
 
 def plotx(sz, x, y, color, lw):
+    rad = sz/2
     sz /= np.sqrt(2)
     plt.plot([x+sz/2, x-sz/2], [y+sz/2,y-sz/2], '-', lw=lw, color=color)
     plt.plot([x+sz/2, x-sz/2], [y-sz/2,y+sz/2], '-', lw=lw, color=color)
-
-    plt.plot([x+sz/2, x+sz/2], [y-sz/2,y-sz/4], '-', lw=lw, color=color)
-    plt.plot([x+sz/2, x+sz/2], [y+sz/2,y+sz/4], '-', lw=lw, color=color)
-    plt.plot([x-sz/2, x-sz/2], [y-sz/2,y-sz/4], '-', lw=lw, color=color)
-    plt.plot([x-sz/2, x-sz/2], [y+sz/2,y+sz/4], '-', lw=lw, color=color)
-
-    plt.plot([x-sz/2, x-sz/4], [y+sz/2,y+sz/2], '-', lw=lw, color=color)
-    plt.plot([x+sz/2, x+sz/4], [y+sz/2,y+sz/2], '-', lw=lw, color=color)
-    plt.plot([x-sz/2, x-sz/4], [y-sz/2,y-sz/2], '-', lw=lw, color=color)
-    plt.plot([x+sz/2, x+sz/4], [y-sz/2,y-sz/2], '-', lw=lw, color=color)
+    r = np.linspace(0.7*rad, rad, 2)
+    for theta in np.arange(np.pi/4, 2*np.pi, np.pi/2):
+        plt.plot(x+r*np.cos(np.linspace(theta-.2, theta,2)),
+                 y+r*np.sin(np.linspace(theta-.2, theta,2)), '-', lw=lw, color=color)
+        plt.plot(x+r*np.cos(np.linspace(theta+.2, theta,2)),
+                 y+r*np.sin(np.linspace(theta+.2, theta,2)), '-', lw=lw, color=color)
 
 def plotpow(sz, x, y, color='w'):
     theta = np.linspace(0, 2*np.pi, 21)
     r = np.zeros_like(theta) + sz/2
     r[::2] /= 2;
     plt.plot(x+.3*r*np.cos(theta), y+.3*r*np.sin(theta), '-', lw=8*sz, color='white')
-    plt.plot(x+.6*r*np.cos(theta), y+.6*r*np.sin(theta), '-', lw=2*sz, color='yellow')
-    plt.plot(x+.8*r*np.cos(theta), y+.8*r*np.sin(theta), '-', lw=2*sz, color='orange')
+    plt.plot(x+.6*r*np.cos(theta), y+.6*r*np.sin(theta), '-', lw=2.5*sz, color='yellow')
+    plt.plot(x+.8*r*np.cos(theta), y+.8*r*np.sin(theta), '-', lw=2.5*sz, color='orange')
     if color == 'red':
-        plt.plot(x+r*np.cos(theta), y+r*np.sin(theta), '-', lw=2*sz, color='orange')
+        plt.plot(x+r*np.cos(theta), y+r*np.sin(theta), '-', lw=2.5*sz, color='orange')
     else:
-        plt.plot(x+r*np.cos(theta), y+r*np.sin(theta), '-', lw=2*sz, color='red')
+        plt.plot(x+r*np.cos(theta), y+r*np.sin(theta), '-', lw=2.5*sz, color='red')
 
 def plotteleport(sz, x, y):
     theta = np.linspace(0, 2*np.pi, 201)
@@ -228,19 +238,19 @@ def symbol(tok, rad, x, y, backcolor):
         plotplus(rad,x,y, color, 2)
     elif tok == '#':
         plotplus(rad,x,y, color, 2)
-        plotpow(0.3,x,y)
+        plotpow(0.6*rad,x,y)
     elif tok == 'x':
         plotx(rad,x,y, color, 2)
     elif tok == 'X':
         plotx(rad,x,y, color, 2)
-        plotpow(0.3,x,y)
+        plotpow(0.6*rad,x,y)
     elif tok == '*':
         plotx(rad,x,y, color, 2)
         plotplus(rad,x,y, color, 2)
     elif tok == '@':
         plotx(rad,x,y, color, 2)
         plotplus(rad,x,y, color, 2)
-        plotpow(0.3,x,y)
+        plotpow(0.6*rad,x,y)
     elif tok == '2':
         gridtwo(rad,x,y, color, 1)
         plotpow(0.7*rad,x,y)
@@ -260,7 +270,7 @@ def symbol(tok, rad, x, y, backcolor):
         plotretaliate(rad,x,y, color)
     elif tok == 'H':
         gridtwo(rad,x,y, color, 1)
-        plotheal(0.4*rad,x,y, color)
+        plotheal(0.3*rad,x,y, color)
     elif tok == 'A':
         plotshield(rad,x,y, color)
     elif tok == 'S':
@@ -279,22 +289,37 @@ def symbol(tok, rad, x, y, backcolor):
         plotpow(rad,x,y)
 
 def positions(side):
-    x = .55
+    sz = 1.8
+    f = 0.95
     if side == 1:
-        return [(0,0)]
+        return [(0,0,f*sz)]
     if side == 2:
-        return [(x,x), (-x,-x)]
+        return [(sz/4,sz/4,f*sz/np.sqrt(2)), (-sz/4,-sz/4,f*sz/np.sqrt(2))]
     if side == 3:
-        return [(x,-x), (-x,-x), (0,x)]
+        return [(sz/4,-sz/4,f*sz/2), (-sz/4,-sz/4,f*sz/2), (0,sz/4,f*sz/2)]
     if side == 4:
-        return [(x,-x), (-x,-x), (x,x), (-x,x)]
+        return [(sz/4,-sz/4,f*sz/2), (-sz/4,-sz/4,f*sz/2),
+                (sz/4,sz/4,f*sz/2), (-sz/4,sz/4,f*sz/2)]
     if side == 5:
-        return [(x,-x), (-x,-x), (x,x), (-x,x), (0,0)]
+        p = []
+        z = 0.6
+        r = sz/2 - z/2
+        for i in reversed(range(5)):
+            p.append((r*np.sin(i*2*np.pi/5), r*np.cos(i*2*np.pi/5), z))
+        return p
+        # return [(x,-x,1/np.sqrt(2)), (-x,-x,1/np.sqrt(2)),
+        #         (x,x,1/np.sqrt(2)), (-x,x,1/np.sqrt(2)), (0,0,1/np.sqrt(2))]
     if side == 6:
-        return [(x,-x), (-x,-x), (x,x), (-x,x), (x,0), (-x,0)]
+        z = 0.5
+        r = sz/2 - z/2
+        p = [(0,0,(sz-2*z))]
+        for i in reversed(range(5)):
+            p.append((r*np.sin(i*2*np.pi/5), r*np.cos(i*2*np.pi/5), z))
+        return p
 
 def plotme(m, side):
     name = m['name']
+    plt.close('all')
     plt.figure(figsize=(2,2))
     plt.xlim(-imsz,imsz)
     plt.ylim(-imsz,imsz)
@@ -303,15 +328,11 @@ def plotme(m, side):
     if m['color'] in ['y', 'yellow']:
         directioncolor = 'black'
     plt.gca().set_facecolor(m['color'])
-    rad = 1.0
     for i in range(side):
-        x,y = positions(side)[i]
-        symbol(m['move'][i],rad - (rad-.48)*side/6,x,y,m['color'])
-    y = 0
-    if side in [1,5]:
-        y = -.6
+        x,y,rad = positions(side)[i]
+        symbol(m['move'][i],rad,x,y,m['color'])
     for w in np.arange(0.03, .9, 0.03):
-        text = plt.text(0, y, name, color=directioncolor,
+        text = plt.text(0, 0, name, color=directioncolor,
                         ha='center', va='center', size=14)
         text.set_path_effects([path_effects.Stroke(linewidth=1/w, alpha=w, foreground=m['color']),
                            path_effects.Normal()])
@@ -323,15 +344,15 @@ for monster in monsters:
         plt.savefig('sides/{}-{}.png'.format(monster['name'],side), dpi=94)
 
 for action in actions:
-    rad = 1.0
     for side in range(1,7):
+        plt.close('all')
         plt.figure(figsize=(2,2))
         plt.xlim(-imsz,imsz)
         plt.ylim(-imsz,imsz)
         plt.gca().set_position([0, 0, 1, 1])
         plt.gca().set_facecolor(action['color'])
-        for (x,y) in positions(side):
-            symbol(action['sides'][side-1],rad - (rad-.48)*side/6,x,y,action['color'])
+        for x,y,rad in positions(side):
+            symbol(action['sides'][side-1],rad,x,y,action['color'])
         plt.savefig('sides/{}-{}.png'.format(action['color'],side), dpi=94)
 
 with open('paper.tex'.format(monster,side),'w') as f:
